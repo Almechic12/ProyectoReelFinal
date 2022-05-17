b7e6ba5010c27bfd65778f18577b57a67afc5203
Utiak <utiak.covarrubias@potros.itson.edu.mx>
2022-05-17 01:13:28 -0700

    EstrenosAdapter agregado, clase Estreno creada, se agregaron varias imagenes de trailers

--- app/src/main/java/gonzalez/edith/proyectofinalreel/DatosFragment.kt
+++ app/src/main/java/gonzalez/edith/proyectofinalreel/DatosFragment.kt
@@ -25,12 +25,14 @@ class DatosFragment(val bundle: Bundle?) : Fragment() {
         val tv_peli_titulo: TextView = vista.findViewById(R.id.tv_peli_titulo)
         val tv_peli_titulo_ingles: TextView = vista.findViewById(R.id.tv_peli_titulo_ingles)
         val rating: RatingBar = vista.findViewById(R.id.estrellas)
+        val trailer: ImageView = vista.findViewById(R.id.img_trailer)
         val sinopsis: TextView = vista.findViewById(R.id.tv_sinopsis)
 
         tv_peli_titulo.text = bundle?.getString("nombre")
         tv_peli_titulo_ingles.text = bundle?.getString("nombre_ingles")
         img_pelicula.setImageResource(bundle?.getInt("image")!!)
         rating.rating = bundle?.getFloat("estrellas")!!
+        trailer.setImageResource(bundle?.getInt("trailer"))
         sinopsis.text = bundle?.getString("sinopsis")
 
         return vista
