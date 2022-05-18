941a6919c2f26719933be86cf5a3aae0689ef14d
Almechic12 <ediithcarbherre25@gmail.com>
2022-05-18 06:32:32 -0700

    queriendo hacer estrenos con video

--- app/src/main/java/gonzalez/edith/proyectofinalreel/videoModel.java
+++ app/src/main/java/gonzalez/edith/proyectofinalreel/videoModel.java
@@ -1,6 +1,17 @@
 package gonzalez.edith.proyectofinalreel;
 
 public class videoModel {
+ String videoUrl;
 
+    public videoModel(String vidUrl){
+        this.videoUrl = vidUrl;
+    }
 
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
 }
